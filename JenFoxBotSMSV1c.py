2bfe4f21299b1a69e2c7cdffdcf1a72113489bcc
Joe Elliott <joe.elliott@joulebug.com>
2015-05-09 21:12:59 -0400

    Some refactoring of the counting of measurements.  Change percent to water to 30%

--- JenFoxBotSMSV1c.py
+++ JenFoxBotSMSV1c.py
@@ -8,6 +8,11 @@ file = open("SensorData.txt", "w") #stores data file in same directory as this p
 #TODO: feedback loops to adjust these values?
 airTimeConstant = 3.45
 waterTimeConstant = 5.00
+#when the soil is 30% between air and water, it's too dry.
+dryPercentage = 0.3
+soilTooDry = (waterTimeConstant - airTimeConstant)*dryPercentage + airTimeConstant
+time_start = time.time()
+counter = 0
 
 #Define function to measure charge time
 def RC_Analog(Pin):
@@ -32,29 +37,28 @@ def RC_Analog(Pin):
 
     #Main program loop
 while True:
+
     time.sleep(1)
     ts = time.time()
     reading = RC_Analog(4) #store counts in a variable
-    counter = 0
-    time_start = 0
-    time_end = 0
     
     print ts, reading  #print counts using GPIO4 and time
     file.write(str(ts) + " " + str(reading) + "\n") #write data to file
 
-    #when the soil is 60% between air and water, it's too dry.
-    dryPercentage = 0.6
-    soilTooDry = (waterTimeConstant - airTimeConstant)*dryPercentage + airTimeConstant
-    while (reading < soilTooDry):
-        time_start = time.time()
+    if(reading < soilTooDry):
         counter = counter + 1
-        if counter >= 50:
-            break
+
     time_end = time.time()
-    if (counter >= 25 and (time_end - time_start) <= 60): # if you get 25 measurements that indicate dry soil in less than one minute, need to water
+
+    if (counter >= 25): # if you get 25 measurements that indicate dry soil in less than one minute, need to water
         print('Not enough water for your plants to survive! Please water now.') #comment this out for testing
     else:
         print('Your plants are safe and healthy, yay!')
+
+    #reset the counter every 60 seconds.
+    if ((time_end - time_start) > 60):
+        time_start = time.time()
+        counter = 0
 
 GPIO.cleanup()
 file.close()
