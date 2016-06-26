7e3433cfabb6fd094e7a8350b2aa054546afa192
Joe Elliott <joe.elliott@joulebug.com>
2016-06-26 14:41:03 -0400

    Added function to send daily email if plants need water.

--- JenFoxBotSMSV1c.py
+++ JenFoxBotSMSV1c.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import time
+import smtplib
+import os
 GPIO.setmode(GPIO.BCM)
 
 file = open("SensorData.txt", "w") #stores data file in same directory as this program file
@@ -15,28 +17,64 @@ time_start = time.time()
 counter = 0
 needsWatering = 0
 
+#given a date, return yearmonthday
+#April 4, 1987 -> 19870404
+def year_month_day(date):
+
+    return int(str(date.year()) + str(date.month()).zfill(2) + str(date.day()).zfill(2))
+
+def send_email():
+
+    smtpUser = os.environ.get('SMTP_USER')
+    smtpPass = os.environ.get('SMTP_PASSWORD')
+
+    toAdd = os.environ.get('NOTIFY_USER_EMAIL')
+
+    fromAdd = smtpUser
+    subject = 'Plants Need Water!'
+    header = "to: "
+    body = "Your plants need water.  The Raspberry Pi moisture sensor detected the water level was too low."
+
+    s = smtplib.SMTP('smtp.gmail.com',587)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+
+    s.login(smtpUser,smtpPass)
+    s.sendmail(fromAdd,toAdd,header + '\n' + body)
+    s.quit()
+
+
+#today's year, month and day.
+#later check to see if yearmonthday is > current yearmonthday.  if so, flip daily_email to false.
+todays_ymd = year_month_day(time_start)
+daily_email_sent = False
+
 #Define function to measure charge time
+#Take a reading from the moisture sensor.
 def RC_Analog(Pin):
-    counter=0
-    sleepTime = 0.1
+    counter_inner=0
+    sleep_time = 0.1
 
     #I've found that the capacitance change is really small, so let's multiply it to get numbers > 1.
-    fudgeFactor = 10000
+    fudge_factor = 10000
     start_time = time.time()
     #Discharge capacitor
     GPIO.setup(13, GPIO.OUT)
     GPIO.output(13, GPIO.LOW)
-    time.sleep(sleepTime) #in seconds, suspends execution.
+    time.sleep(sleep_time) #in seconds, suspends execution.
     GPIO.setup(13, GPIO.IN)
 #Count loops until voltage across capacitor reads high on GPIO
-    while (GPIO.input(13)==GPIO.LOW):
-        counter=counter+1
+    while GPIO.input(13)==GPIO.LOW:
+        counter_inner=counter_inner+1
     end_time = time.time()
     # print counter
-    return ((end_time - start_time)-sleepTime)*fudgeFactor
+    return ((end_time - start_time)-sleep_time)*fudge_factor
 
 
-    #Main program loop
+
+
+ #Main program loop
 while True:
 
     time.sleep(1)
@@ -47,26 +85,35 @@ while True:
     file.write(str(ts) + " " + str(reading) + "\n") #write data to file
 
 
-    if(not needsWatering and reading < soilTooDry):
+    if not needsWatering and reading < soilTooDry:
         counter = counter + 1
-    elif(needsWatering and reading >= soilTooDry):
+    elif needsWatering and reading >= soilTooDry:
         counter = counter + 1
 
 
     time_end = time.time()
 
-    if(counter >= 25):
+    if counter >= 25:
         needsWatering = not needsWatering
 
-    if (needsWatering): # if you get 25 measurements that indicate dry soil in less than one minute, need to water
+    if needsWatering: # if you get 25 measurements that indicate dry soil in less than one minute, need to water
         print('Not enough water for your plants to survive! Please water now.') #comment this out for testing
+        if not daily_email_sent:
+            send_email()
+            daily_email_sent = True
+
     else:
         print('Your plants are safe and healthy, yay!')
 
     #reset the counter every 60 seconds.
-    if ((time_end - time_start) > 60):
+    if (time_end - time_start) > 60:
         time_start = time.time()
         counter = 0
+
+    if todays_ymd < year_month_day(ts):
+
+        todays_ymd = year_month_day(ts)
+        daily_email_sent = False
 
 GPIO.cleanup()
 file.close()
